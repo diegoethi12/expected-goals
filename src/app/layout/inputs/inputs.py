d40d8fd6d2af5afda91f0c3cc616d5c8b6fbbbbb
diego lopez <diegoethi12@gmail.com>
2021-08-17 19:58:16 -0400

    refactor(inputs): create inputs layout

---
+++ src/app/layout/inputs/inputs.py
@@ -0,0 +1,77 @@
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+
+import numpy as np
+import pandas as pd
+
+from src import DATA_PATH
+
+# Config
+NUMERICAL_FEATURES = ['distance', 'angle']
+CATEGORICAL_FEATURES = ['play_pattern', 'shot_body_part', 'shot_technique']
+
+shots = pd.read_pickle(DATA_PATH / 'open_shots.pkl')
+dd_options = {
+    var: [dict(label=category, value=category) for category in shots[var].unique()] for var in CATEGORICAL_FEATURES
+}
+
+
+def create_dropdown_form_group(label, id, variable, description):
+    return dbc.FormGroup([
+        dbc.Label(label, html_for=id),
+        dcc.Dropdown(
+            id=id,
+            options=dd_options.get(variable),
+            value=dd_options.get(variable)[0].get('value'),
+            placeholder=description,
+        )
+    ])
+
+
+play_pattern_form = create_dropdown_form_group(
+    label='Play pattern',
+    id='play-pattern-dropdown',
+    variable='play_pattern',
+    description='Select a play pattern'
+)
+
+shot_body_part_form = create_dropdown_form_group(
+    label='Shot body part',
+    id='shot-body-part-dropdown',
+    variable='shot_body_part',
+    description='Select a shot body part'
+)
+
+shot_technique_form = create_dropdown_form_group(
+    label='Shot technique',
+    id='shot-technique-dropdown',
+    variable='shot_technique',
+    description='Select a shot technique'
+)
+
+angle_form = dbc.FormGroup([
+    dbc.Label('Shot Angle', html_for='angle-slider'),
+    dcc.Slider(
+        id='angle-slider',
+        min=0,
+        max=1,
+        value=0.4,
+        marks={str(round(angle, 1)): str(round(angle, 1)) for angle in np.arange(0, 1.1, 0.1)},
+        step=None
+    )
+])
+
+distance_form = dbc.FormGroup([
+    dbc.Label('Distance', html_for='distance-slider'),
+    dcc.Slider(
+        id='distance-slider',
+        min=0,
+        max=100,
+        value=20,
+        marks={str(round(distance, 1)): str(round(distance, 1)) for distance in range(0, 101, 10)},
+        step=None
+    )
+])
+
+
+inputs = dbc.Form([play_pattern_form, shot_body_part_form, shot_technique_form, angle_form, distance_form])
--- src/app/layout/layout.py
+++ src/app/layout/layout.py
@@ -3,21 +3,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from joblib import load
-import pandas as pd
-import numpy as np
 
 from src.app.components.pitch import plot_pitch
-from src import DATA_PATH, MODEL_PATH
+from src import MODEL_PATH
+from src.app.layout.inputs.inputs import inputs
 
 # Data and model
-NUMERICAL_FEATURES = ['distance', 'angle']
-CATEGORICAL_FEATURES = ['play_pattern', 'shot_body_part', 'shot_technique']
-
-shots = pd.read_pickle(DATA_PATH / 'open_shots.pkl')
-dd_options = {
-    var: [dict(label=category, value=category) for category in shots[var].unique()] for var in CATEGORICAL_FEATURES
-}
-
 xg_model = load(MODEL_PATH / 'model.joblib')
 
 # Components
@@ -26,67 +17,6 @@ description = '''
     built over the [open shots data](https://github.com/statsbomb/statsbombpy) of the 2018 FIFA World Cup
     provided by StatsBomb. Also, you can visit the [source code](https://github.com/diegoethi12) of the app.
 '''
-
-
-def create_dropdown_form_group(label, id, variable, description):
-    return dbc.FormGroup([
-        dbc.Label(label, html_for=id),
-        dcc.Dropdown(
-            id=id,
-            options=dd_options.get(variable),
-            value=dd_options.get(variable)[0].get('value'),
-            placeholder=description,
-        )
-    ])
-
-
-play_pattern_form = create_dropdown_form_group(
-    label='Play pattern',
-    id='play-pattern-dropdown',
-    variable='play_pattern',
-    description='Select a play pattern'
-)
-
-shot_body_part_form = create_dropdown_form_group(
-    label='Shot body part',
-    id='shot-body-part-dropdown',
-    variable='shot_body_part',
-    description='Select a shot body part'
-)
-
-shot_technique_form = create_dropdown_form_group(
-    label='Shot technique',
-    id='shot-technique-dropdown',
-    variable='shot_technique',
-    description='Select a shot technique'
-)
-
-angle_form = dbc.FormGroup([
-    dbc.Label('Shot Angle', html_for='angle-slider'),
-    dcc.Slider(
-        id='angle-slider',
-        min=0,
-        max=1,
-        value=0.4,
-        marks={str(round(angle, 1)): str(round(angle, 1)) for angle in np.arange(0, 1.1, 0.1)},
-        step=None
-    )
-])
-
-distance_form = dbc.FormGroup([
-    dbc.Label('Distance', html_for='distance-slider'),
-    dcc.Slider(
-        id='distance-slider',
-        min=0,
-        max=100,
-        value=20,
-        marks={str(round(distance, 1)): str(round(distance, 1)) for distance in range(0, 101, 10)},
-        step=None
-    )
-])
-
-
-inputs = dbc.Form([play_pattern_form, shot_body_part_form, shot_technique_form, angle_form, distance_form])
 
 layout = dbc.Container(
     [
